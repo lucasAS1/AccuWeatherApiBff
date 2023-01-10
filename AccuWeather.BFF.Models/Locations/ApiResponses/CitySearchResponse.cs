f29c87b2614f7f77e57d1be899b9005755e4450f
lucasAS1 <lucas.a.silva1@outlook.com>
2023-01-10 20:19:00 -0300

    resolvendo code smells

--- AccuWeather.BFF.Models/Locations/ApiResponses/CitySearchResponse.cs
+++ AccuWeather.BFF.Models/Locations/ApiResponses/CitySearchResponse.cs
@@ -5,14 +5,14 @@ namespace AccuWeather.BFF.Models.Locations.ApiResponses;
 [ExcludeFromCodeCoverage]
 public class CitySearchResponse
 {
-    public readonly int Version;
+    public readonly int? Version;
     public readonly string? Key;
     public readonly string? Type;
-    public readonly int Rank;
+    public readonly int? Rank;
     public readonly string? LocalizedName;
     public readonly string? EnglishName;
     public readonly string? PrimaryPostalCode;
-    public readonly Region Region;
-    public readonly Country Country;
-    public readonly AdministrativeArea AdministrativeArea;
+    public readonly Region? Region;
+    public readonly Country? Country;
+    public readonly AdministrativeArea? AdministrativeArea;
 }
